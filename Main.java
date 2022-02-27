accc46281b27a06051c8dafbc536aec994a569c5
Shik2003 <100432857+Shik2003@users.noreply.github.com>
2022-02-27 12:31:24 +0530

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,13 @@
+
+public class Main extends GuessTheNumberGame2{
+	  public static void
+	    main(String arg[])
+	    {
+	 
+	        // Function Call
+	        guessingNumberGame();
+	    }
+	    
+	   
+
+}
